ed88a28b718c0a06ff51a86e4d8bf92aea451536
razdvapoka <razdvapoka@gmail.com>
2018-08-24 12:38:53 +0300

    :construction: Draft intro's arrow's drawing animation

--- pages/transition.js
+++ pages/transition.js
@@ -1,6 +1,6 @@
 import FlexBox from '../components/flexbox'
 import { lifecycle } from 'recompose'
-// import anime from 'animejs'
+import anime from 'animejs'
 
 let Intro1 = () => (
   <svg width='60%' height='60%' viewBox='0 0 1310 600'>
@@ -21,7 +21,7 @@ let Intro1 = () => (
         <tspan x='194' y='577'>СООБЩЕСТВА</tspan>
       </text>
       <g stroke='#FFF' strokeWidth='16' transform='translate(705 220)'>
-        <path id='#line' d='M459.019355,154 L521.613004,154 C564.138874,154 598.612903,119.525971 598.612903,77.0001003 C598.612903,34.4742299 564.138874,0.000200555637 521.613004,0.000200555637 C493.262423,0.000200555637 319.391422,0.000133703758 0,0' />
+        <path id='line' d='M 0.0,0.0 C 319.391422,0.000133703758 493.262423,0.000200555637 521.613004,0.000200555637 C 564.138874,0.000200555637 598.612903,34.4742299 598.612903,77.0001003 C 598.612903,119.525971 564.138874,154.0 521.613004,154.0 L 459.019355,154.0' />
         <polyline points='511.06 97 455 154.002 511.06 211' />
       </g>
       <path stroke='#FFF' strokeLinecap='square' strokeWidth='16' d='M19.5,551.5 L167.5,551.5' transform='translate(0, -35)' />
@@ -31,16 +31,15 @@ let Intro1 = () => (
 
 Intro1 = lifecycle({
   componentDidMount () {
-    // const pathEl = document.querySelector(`#line`)
-    // const offset = anime.setDashoffset(pathEl)
-    // pathEl.setAttribute('stroke-dashoffset', offset)
-    // anime({
-    //   targets: pathEl,
-    //   strokeDashoffset: [ offset, 0 ],
-    //   duration: 3000,
-    //   easing: 'linear',
-    //   delay: 1000
-    // })
+    const pathEl = document.querySelector(`#line`)
+    const offset = anime.setDashoffset(pathEl)
+    pathEl.setAttribute('stroke-dashoffset', offset)
+    anime({
+      targets: pathEl,
+      strokeDashoffset: [ offset, 0 ],
+      duration: 1000,
+      easing: 'easeInQuad'
+    })
   }
 })(Intro1)
 
