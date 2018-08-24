9f91dec4d9c72f3b6e3f8a95e65e6e6e9dbec637
razdvapoka <razdvapoka@gmail.com>
2018-08-24 12:15:47 +0300

    :fire: Remove old commented code

--- components/line.js
+++ components/line.js
@@ -1,21 +1,6 @@
 import Dash from './dash'
-// import {
-//   LEFT_MARGIN,
-//   RIGHT_MARGIN
-// } from './consts'
-import { compose, withProps } from 'recompose'
+import { withProps } from 'recompose'
 
-// const marginProp = ({ margin }) => {
-//   switch (margin) {
-//     case LEFT_MARGIN: return { mgl: 1, mgr: 0 }
-//     case RIGHT_MARGIN: return { mgl: 0, mgr: 1 }
-//     default: return {}
-//   }
-// }
-
-const Line = compose(
-  // withProps(marginProp),
-  withProps({ flex: 1 })
-)(Dash)
+const Line = withProps({ flex: 1 })(Dash)
 
 export default Line
